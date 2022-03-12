7b58d20728a945a0925563b972f3c8517d9d5abb
XVolik <xvolik@gmail.com>
2022-03-12 09:57:31 +0200

    test

--- common.js
+++ common.js
@@ -61,4 +61,4 @@
 const a = Number(prompt('Введите a'));
 const b = Number(prompt('Введите b'));
 const c = Number(prompt('Введите c'));
-// console.log(Math.max(a,b,c), Math.min(a,b,c));
+console.log(Math.max(a,b,c), Math.min(a,b,c));
