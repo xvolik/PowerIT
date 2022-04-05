63ec6422f45034a172ea41a361fb88d94018bdb0
XVolik <xvolik@gmail.com>
2022-04-05 19:26:12 +0300

    update 05.04.2022

--- js/common.js
+++ js/common.js
@@ -253,78 +253,128 @@
 // Написать следующие функции обработки этого массива:
 //     считает среднюю оценку и выводит имя и среднюю оценку для каждого элемента массива +
 // выводит учеников со у которых средняя оценка < 5 +
-// находит учеников с максимальной и минимальной оценкой
-// сортирует учеников по средней оценке по убыванию (с самой большой средней в начале, с наименьшим в конце списка)
-// выводит тех учеников чья средняя оценка больше средней оценки всего класса
-
-let students = [
-     {
-        name: "Alex",
-        marks: [8, 10, 7, 5, 4],
-    },
-    {
-        name: "Serghei",
-        marks: [9, 10, 7, 7, 10],
-    },
-    {
-        name: "Marina",
-        marks: [6, 9, 4, 5, 10],
-    },
-    {
-        name: "Ruslan",
-        marks: [9, 10, 10, 8, 10],
-    },
-     {
-        name: "Maxim",
-        marks: [5, 4, 6, 3, 2],
-    },
-];
-let minmax = [];
-function score () {
-students.forEach(function (item) {
-    let result = item.marks.reduce((sum, current) => sum + current, 0);
-    let media = result / item.marks.length;
-    item['media'] = media;
-
-    // function minmaxfunc (media) {
-    //     minmax.push({name, media});
-    // }
-    // minmaxfunc(media);
-    function minmaxfunc (media) {
-        minmax.push(media);
-    }
-    minmaxfunc(media);
-    console.log(item.name +': '+ media);
-    if (media < 5){
-        console.log('Оценка меньше 5 у: ' + item.name +': '+ media + ' балла');
-    }
-    });
-    function SortArray(x, y){
-        if (x.media < y.media) {return -1;}
-        if (x.media > y.media) {return 1;}
-        return 0;
-    }
-    let s = minmax.sort(SortArray);
-    console.log(s);
-    for (let i = 0; i < students.length; i++) {
-        console.log(students[i].media);
-        console.log(typeof (Math.max(minmax)));
-        let a = Math.max(minmax);
-        // Math.min(minmax);
-        if (students[i].media === a) {
-            console.log('Наивысший балл: ' + students[i].name + ' ' + students[i].media);
-        }else {
-            console.log('что-то не так');
-        }
-    }
-console.log(students);
-    console.log(typeof (students[0].media));
-// console.log(minmax);
-    // console.log(Math.max(minmax));
-
-}
-score();
-
-
-
-
+// находит учеников с максимальной и минимальной оценкой +
+// сортирует учеников по средней оценке по убыванию (с самой большой средней в начале, с наименьшим в конце списка)+
+// выводит тех учеников чья средняя оценка больше средней оценки всего класса +
+//
+// let students = [
+//      {
+//         name: "Alex",
+//         marks: [8, 10, 7, 5, 4],
+//     },
+//     {
+//         name: "Serghei",
+//         marks: [9, 10, 7, 7, 10],
+//     },
+//     {
+//         name: "Marina",
+//         marks: [6, 9, 4, 5, 10],
+//     },
+//     {
+//         name: "Ruslan",
+//         marks: [9, 10, 10, 8, 10],
+//     },
+//      {
+//         name: "Maxim",
+//         marks: [5, 4, 6, 3, 2],
+//     },
+// ];
+// let classMedia = [];
+// let studentsList = [];
+// let lessFiveList = [];
+// function mainFunc () {
+// students.forEach(function (item) {
+//     let result = item.marks.reduce((sum, current) => sum + current, 0);
+//     let media = result / item.marks.length;
+//     item['media'] = media;
+//     classMedia.push(media);
+//     studentsList.push(item.name +': '+ media);
+//     });
+//
+// // console.log(studentsList);
+// //
+// //     let xo = classMedia.reduce ((sum, current) => sum + current, 0);
+// //     let resultXo = xo / classMedia.length;
+// //     console.log("Общий средний балл : " + resultXo );
+//     // function sortByMedia(x, y) {
+//     //     return y['media'] - x['media']
+//     // }
+//     // console.log(students.sort(sortByMedia));
+//     // console.log('Наивысший балл: ' + students[0].name + ' ' + students[0].media);
+//     // console.log('Наименьший балл: ' + students[students.length - 1].name + ' ' + students[students.length - 1].media);
+//     // console.log(classMedia);
+//     // const bests = students.filter(student =>{
+//     //     if (student.media >= resultXo) {
+//     //         return true
+//     //     }
+//     // })
+//     // let i = 0;
+//     // while (i < bests.length) {
+//     //     console.log("Топ класса\n" + bests[i].name +': '+ bests[i].media);
+//     //     i++;
+//     // }
+//
+//     function showStudentList() {
+//         console.log("Имя и средний балл студентов: ");
+//         let i = 0;
+//         while (i < studentsList.length){
+//             console.log(studentsList[i])
+//             i++
+//         }
+//     }
+//     showStudentList();
+//
+//     function lessFive() {
+//         console.log("Список студентов с оценкой ниже 5: ");
+//         let i = 0;
+//         while (i < lessFiveList.length){
+//             console.log(lessFiveList[i]);
+//             i++
+//     }
+//         //случайно сделал еще раз фильтр оценки ниже 5 ((((
+//         // console.log("Список студентов с оценкой ниже 5: ");
+//         // let i = 0;
+//         // while (i < students.length){
+//         //     if (students[i].media < 5) {
+//         //         console.log(students[i].name + ' ' + students[i].media);
+//         //     }
+//         //     i++
+//         // }
+//     }
+//     lessFive();
+//
+//     function highLow() {
+//         console.log('Наивысший балл: ' + students[0].name + ' ' + students[0].media);
+//         console.log('Наименьший балл: ' + students[students.length - 1].name + ' ' + students[students.length - 1].media);
+//     }
+//     highLow()
+//
+//     function sortByMedia() {
+//         function sorter(x, y) {
+//             return y['media'] - x['media']
+//         }
+//         students.sort(sorter)
+//         console.log("Сортировка по убыванию :");
+//         students.forEach(element => console.log(element.name + " " + element.media));
+//     }
+//     sortByMedia();
+//
+//     function topStudents() {
+//             let classMediaSumm = classMedia.reduce ((sum, current) => sum + current, 0);
+//             let commonMedia = classMediaSumm / classMedia.length;
+//         const bests = students.filter(student =>{
+//             if (student.media >= commonMedia) {
+//                 return true
+//             }
+//         })
+//         console.log("Топ класса:")
+//         let i = 0;
+//         while (i < bests.length) {
+//             console.log(bests[i].name +': '+ bests[i].media);
+//             i++;
+//         }
+//     }
+//     topStudents();
+// }
+//
+// mainFunc();
