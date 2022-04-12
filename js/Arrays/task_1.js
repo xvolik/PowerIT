3da22d10163660b35959993e34ec7ee464c9108b
XVolik <xvolik@gmail.com>
2022-04-12 16:04:03 +0300

    some corrections

--- js/Arrays/task_1.js
+++ js/Arrays/task_1.js
@@ -6,39 +6,29 @@ function task2() {
     for (let i = a; i <= b; i++) {
         someArray.push(i);
     }
-    //Цикл for
     for (let i = 0; i < someArray.length; i++) {
         console.log(someArray[i]);
     }
-    // Цикл for of
     for (let someArrayItem of someArray) {
         console.log(someArrayItem);
     }
-    // Цикл while
     let i = 0;
     while (i < someArray.length) {
         console.log(someArray[i]);
         i++;
     }
-    // Цикл do ... while
     let x = 0;
     do {
         console.log(someArray[x]);
         x++;
     } while (x < someArray.length);
-    //Цикл for in
     for (let key in someArray) {
         console.log(someArray[key]);
     }
-    // Цикл forEach
     someArray.forEach(element => console.log(element));
-    //
-    // .map
     someArray.map(function (x, i) {
         console.log(i, x)
     });
-    //
-    // .reduce
     someArray.reduce(function (previousValue, item) {
         console.log(item);
     }, 0)
